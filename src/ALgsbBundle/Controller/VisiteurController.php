86a8d54161e735c55d9c0d3dae08adf9ac3c4e61
laze <laze@G14D0S>
2018-05-01 15:50:30 +0200

    affichage de la page d'accueil du visiteur

--- src/ALgsbBundle/Controller/VisiteurController.php
+++ src/ALgsbBundle/Controller/VisiteurController.php
@@ -14,8 +14,37 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class VisiteurController extends Controller {
     
+    /**
+     * Affichage de la page d'accueil du visiteur
+     * 
+     * @param Request $request
+     * @return VisiteurController
+     */
     public function accueilAction(Request $request)
     {
-        return $this->render('@ALgsb/Visiteur/accueil_visiteur.html.twig');
+        $id = $request->attributes->get('id');
+        
+        $leComptable = $this->getVisiteur($id);
+        
+        $nom = $leComptable->getNom();
+        $prenom = $leComptable->getPrenom();
+        
+        return $this->render('@ALgsb/Visiteur/accueil_visiteur.html.twig', array('id'=>$id, 'nom'=>$nom, 'prenom'=>$prenom));
+    }
+    
+    
+    /**
+     * Retourne les informations sur le visiteur en fonction de l'identifiant
+     * 
+     * @param str $id
+     * @return Visiteur
+     */
+    private function getVisiteur($id)
+    {
+        return $this
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('ALgsbBundle:Visiteur')
+                ->find($id);
     }
 }
