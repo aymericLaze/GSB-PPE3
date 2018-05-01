cc32208b7dd6ced377eb3c25660ace56c903ec5f
laze <laze@G14D0S>
2018-05-01 15:43:51 +0200

    action d'accueil

--- src/ALgsbBundle/Controller/ComptableController.php
+++ src/ALgsbBundle/Controller/ComptableController.php
@@ -16,6 +16,28 @@ class ComptableController extends Controller{
     
     public function accueilAction(Request $request)
     {
-        return $this->render('@ALgsb/Comptable/accueil_comptable.html.twig');
+        $id = $request->attributes->get('id');
+        
+        $leComptable = $this->getComptable($id);
+        
+        $nom = $leComptable->getNom();
+        $prenom = $leComptable->getPrenom();
+        
+        return $this->render('@ALgsb/Comptable/accueil_comptable.html.twig', array('id'=>$id, 'nom'=>$nom, 'prenom'=>$prenom));
+    }
+    
+    /**
+     * Retourne les informations sur le comptable en fonction de l'identifiant
+     * 
+     * @param str $id
+     * @return Comptable
+     */
+    private function getComptable($id)
+    {
+        return $this
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('ALgsbBundle:Comptable')
+                ->find($id);
     }
 }
