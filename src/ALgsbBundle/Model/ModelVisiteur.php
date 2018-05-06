5cbc896248860b59b3f0b41cc0df7816c1fea092
laze <laze@G14D0S>
2018-05-07 01:38:48 +0200

    ajout des methodes du visiteur

--- src/ALgsbBundle/Model/ModelVisiteur.php
+++ src/ALgsbBundle/Model/ModelVisiteur.php
@@ -2,11 +2,64 @@
 
 namespace ALgsbBundle\Model;
 
+use ALgsbBundle\Entity\Fichefrais;
+use ALgsbBundle\Entity\Lignefraishorsforfait;
+
 /**
- * Description of ModelVisiteur
+ * Methodes specifiques du visiteur
  *
  * @author laze
  */
 class ModelVisiteur {
     
+    /**
+     * Retourne un tableau pour créer le formulaire de sélection de mois
+     * 
+     * @param array $lesFiches
+     * @return array
+     */
+    public static function getLesMoisPourFormulaire(array $lesFiches) : array
+    {
+        $lesMois = [];
+        
+        // parcours des fiches et creation d'un dictionnaire
+        foreach($lesFiches as $uneFiche)
+        {
+            $mois = $uneFiche->getMois();
+            $moisKey = $mois[4].$mois[5].'-'.$mois[0].$mois[1].$mois[2].$mois[3];
+            $lesMois[$moisKey] = $uneFiche->getId();
+        }
+        
+        return $lesMois;
+    }
+    
+    /**
+     * Verification que la fiche du mois existe
+     * 
+     * @param Fichefrais $laFiche
+     * @return bool
+     */
+    public static function isFicheDuMois(Fichefrais $laFiche) : bool
+    {
+        return $laFiche->getIdetat()->getId() == 'CR';
+    }
+    
+    /**
+     * Enregistre les frais hors forfait du visiteur
+     * 
+     * @param type $leFormulaire
+     * @param Fichefrais $laFiche
+     * @return Lignefraishorsforfait
+     */
+    public static function enregistrerFraisHorsForfait($leFormulaire, Fichefrais $laFiche) : Lignefraishorsforfait
+    {
+        $frais = new Lignefraishorsforfait();
+        
+        $frais->setDate($leFormulaire->get('date')->getData());
+        $frais->setLibelle($leFormulaire->get('libelle')->getData());
+        $frais->setMontant($leFormulaire->get('montant')->getData());
+        $frais->setIdfichefrais($laFiche);
+       
+        return $frais;
+    }
 }
